12857250f0b442379e7577c6eeca5972464e7aa0
liangs <liangshuang@jetair.com.cn>
2022-01-06 22:11:28 +0800

    day 1

--- src/01-getUrlParams/getUrlParams.des.js
+++ src/01-getUrlParams/getUrlParams.des.js
@@ -4,4 +4,20 @@
  * output: getUrlParams('a') => return 1
  */
 
-function getUrlParams() {}
+function getUrlParams(params) {
+  const url = "https://baidu.com?a=1&b=2&c=3";
+  let theRequest = {};
+  const index = url.indexOf("?");
+  if (index !== -1) {
+    const str = url.slice(index + 1);
+    const strsArr = str.split("&");
+    for (let i = 0; i < strsArr.length; i++) {
+      theRequest[strsArr[i].split("=")[0]] = decodeURIComponent(
+        strsArr[i].split("=")[1]
+      );
+    }
+  }
+  return theRequest[params];
+}
+
+console.log(getUrlParams("a"));

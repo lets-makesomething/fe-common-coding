1f49780f896ef46b50a14a8d5de89341416e3abe
Your Name <1659856121@qq.com>
2022-01-08 20:54:03 +0800

    03-add-kangxiaoqiang

---
+++ src/03-add/add-kangxiaoqiang.js
@@ -0,0 +1,15 @@
+/**
+ * @day3
+ * add(1,2) => return 3
+ * add(1)(2) => return 3
+ */
+
+
+function add(num1, num2) {
+    if (num1 === undefined) return transferNum
+    if (num1 !== undefined && num2 === undefined) return x => { return x + num1 }
+    if (num1 !== undefined && num2 !== undefined) return num2 + num1
+    return
+}
+
+console.log(add(1, 2), add(1)(2), add()(1)(2))

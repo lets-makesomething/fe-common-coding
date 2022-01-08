e29643cebb61651d11c14216d7094b3dee5698c8
易沛东 <bndiya@yipeidongs-Mac-mini.local>
2022-01-08 20:24:01 +0800

    03-add-yipeidong

---
+++ src/03-add/add-yipeidong.js
@@ -0,0 +1,11 @@
+/**
+ * @day3
+ * add(1,2) => return 3
+ * add(1)(2) => return 3
+ */
+
+function add() {
+  return arguments.length >= 2 ? (arguments[0] + arguments[1]) : add.bind( this, ...arguments)
+}
+
+console.log(add()(1)()(2))

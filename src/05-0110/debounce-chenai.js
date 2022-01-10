3c7ab66ebda78b49ad80b6edb34240d45890eccc
aline.chen <aline.chen@dbappsecurity.com.cn>
2022-01-10 19:59:20 +0800

    05-0110-debounce-chenai

---
+++ src/05-0110/debounce-chenai.js
@@ -0,0 +1,31 @@
+/**
+ * @description 函数防抖
+ */
+
+// 第一种 非立即执行
+// function debounce(func, time) {
+//     let timer = null
+//     return function () {
+//         if (timer) clearTimeout(timer)
+//         let context = this
+//         let args = arguments
+//         setTimeout(() => {
+//             func.call(context, args)
+//         }, time)
+//     }
+// }
+// 第二种 立即执行版
+function debounce(func, time) {
+    let timer = null
+    return function () {
+        let context = this
+        let args = arguments
+        if (timer) clearTimeout(timer)
+        func.call(context, args)
+        setTimeout(() => {
+            timer = null
+        }, time)
+    }
+}
+
+export default debounce

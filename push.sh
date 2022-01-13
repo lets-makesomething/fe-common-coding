2960f587d5557c04f11a9426e3576156f7074f33
pearyman <yananlee@yanandeiMac.local>
2022-01-13 22:41:54 +0800

    feat: update script

--- push.sh
+++ push.sh
@@ -2,11 +2,12 @@
 
 file=.gitignore
 time=$(date "+%Y-%m-%d %H:%M:%S")
-git rm -r --cached .
 
+git pull
 sed '112d' $file > $file.tmp
 mv $file.tmp $file
-git pull
+
+git rm -r --cached .
 git add .
 git commit -m "feat: ${time} new challenge coming"
 git push

79dd2be3693592cb20aea37a262fc2b9e3648f4b
pearyman <yananlee@yanandeiMac.local>
2022-01-13 23:00:45 +0800

    modify script

--- push.sh
+++ push.sh
@@ -5,9 +5,10 @@ time=$(date "+%Y-%m-%d %H:%M:%S")
 
 git pull
 
-git rm -r --cached .
 sed '112d' $file > $file.tmp
 mv $file.tmp $file
+
+git rm -r --cached .
 git add .
 git commit -m "feat: ${time} new challenge coming"
 git push

9885a1d50295052bb45684a426aeffbe68c51fb9
smallsun0122 <lizhuohua@wakedata.com>
2022-01-15 00:03:47 +0800

    feat：处理传入空的异常情况

--- src/08-0113/promiseRace-zhuohua.js
+++ src/08-0113/promiseRace-zhuohua.js
@@ -9,7 +9,11 @@
  */
 
 function promiseRace(promises) {
+  const len = promises.length;
   return new Promise((resolve, reject) => {
+    if (!len) {
+      return;
+    }
     for(let i = 0; i < promises.length; i++) {
       const promise = Promise.resolve(promises[i]);
       promise.then(val => resolve(val), err => reject(err));

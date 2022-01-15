356ac10a630caeecf41f5c3cb28721a34382b93f
易沛东 <bndiya@yipeidongs-Mac-mini.local>
2022-01-15 23:34:55 +0800

    patch-01-getUrlParams

--- src/01-getUrlParams/getUrlParams-yipeidong.js
+++ src/01-getUrlParams/getUrlParams-yipeidong.js
@@ -10,11 +10,13 @@ function getUrlParams( key ) {
   const params = new Map(url.split('?')[1]    // 获取问好后面部分字符串
     .split('&')                               // 将每组参数拆分
     .map(paramStr => paramStr.split('=')))    // 将每组参数组处理成键值对的形式
-  return params.get(key)
+  const returnValue = params.get(key)
+  return Number(returnValue).toString() === returnValue ? Number(returnValue) : returnValue
 }
 
 function getUrlParams2( key ) {
   const paramsStr = url.split('?')[1]    // 获取问好后面部分字符串
   const regExp = new RegExp(`${key}=([^&]*)`)
-  return paramsStr.match(regExp)?.[1]
+  const returnValue = paramsStr.match(regExp)?.[1]
+  return Number(returnValue).toString() === returnValue ? Number(returnValue) : returnValue
 }

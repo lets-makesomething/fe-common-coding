a5ec9449bc05d00307f818d0cc8acdc90e5ed995
aline.chen <aline.chen@dbappsecurity.com.cn>
2022-01-16 22:25:05 +0800

    fix: 02-transferNum-chenai

--- src/02-transferNum/transferNum-chenai.js
+++ src/02-transferNum/transferNum-chenai.js
@@ -3,40 +3,40 @@
  * @params num 123456
  * @return 123,456
  */
-// 第一种方法 正则
-// function transferNum(num) {
-//     if (typeof num !== 'number') return
-//     return num.toString().replace(/\d{1,3}(?=(\d{3})+$)/g, function (s) {
-//         return s + ','
-//     })
-// }
-// 第二种方法 将数字转化为字符串在进行反转，利用正则匹配三位数字，然后在进行反转
-// function transferNum(num) {
-//     if (typeof num !== 'number') return
-//     function reverseStr(num) {
-//         return num.toString().split('').reverse().join('')
-//     }
-//     return reverseStr(reverseStr(num).replace(/(\d{3})+?/g, function (s) {
-//         return s + ",";
-//     }))
-// }
-
-// 第三种 利用while循环，从后向前匹配
-function transferNum(num) {
+// 第一种方法 将数字转化为字符串在进行反转，利用正则匹配三位数字，然后在进行反转
+function transferNum1(num) {
     if (typeof num !== 'number') return
-    let oldStr = num.toString()
-    let newStr = ''
-    let i = oldStr.length - 1
+    // 反转字符串
+    const reverseStr = num => num.toString().split('').reverse().join('')
+
+    let [intergerNum, decimalNum] = num.toString().split('.');
+
+    const str = reverseStr(reverseStr(intergerNum).replace(/(\d{3})+?/g, function (s) {
+        return s + ",";
+    }))
+
+    const intergerStr = str.charAt(0) == ',' ? str.substring(1, str.length) : str
+    return intergerStr + (decimalNum ? `.${decimalNum}` : '')
+}
+
+// 第二种 利用while循环，从后向前匹配
+function transferNum2(num) {
+    if (typeof num !== 'number') return
+    let [intergerNum, decimalNum] = num.toString().split('.');
+    let intergerStr = ''
+    let i = intergerNum.length - 1
     let j = 0
     while (i >= 0) {
         if (++j >= 3 && i !== 0) {
             j = 0
-            newStr = ',' + oldStr[i] + newStr
+            intergerStr = ',' + intergerNum[i] + intergerStr
         } else {
-            newStr = oldStr[i] + newStr
+            intergerStr = intergerNum[i] + intergerStr
         }
         i--
     }
-    return newStr
+
+    return intergerStr + (decimalNum ? `.${decimalNum}` : '')
 }
-export default transferNum
+
+export default transferNum2

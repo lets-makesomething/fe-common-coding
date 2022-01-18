33ae9e2fafde2ba0cb3c42c7658707ad73af5f5b
cinyearchan <cinyearchan@gmail.com>
2022-01-18 15:16:03 +0800

    bugFix: 修正注释

--- src/13-0118/trim-chenzhiwen.js
+++ src/13-0118/trim-chenzhiwen.js
@@ -22,7 +22,7 @@ function trim(str) {
     // 我们常用的空格是 \x20，是标准 ASCII 可见字符 0x20~0x7e 范围内
     // core-js 中 whitespace 的定义
     // \u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF
-    // + 若干
+    // + 若干 >= 1
     // $ 结尾
     return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '')
   }

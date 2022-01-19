f72ff1e58fa985b86ae095a16b4b13469c57f59e
lyka3156 <1210167160@qq.com>
2022-01-19 11:31:19 +0800

    feat: 14-0119-transfer-liuyongkang

--- src/14-0119/transfer-liuyongkang.js
+++ src/14-0119/transfer-liuyongkang.js
@@ -3,13 +3,12 @@
  * eg：hello everyone, this is our first question topic.
  */
 
+// 方式1：字符串+数组的方法实现
 /**
  * 将英文中每个单词的首字母都变成大写。
  * @param {*} str   字符串
  */
-
-// 方式：字符串+数组的方法实现
-function transferWords(str) {
+function transferWords1(str) {
 	if (!str || typeof str !== 'string') return str;
 	// 以空字符分割每个单词
 	const wordArray = str.split(' ');
@@ -19,6 +18,21 @@ function transferWords(str) {
 		.join(' ');
 }
 
-// 测试案例
-const str = 'hello everyone, this is our first question topic.';
-console.log(transferWords(str)); // Hello Everyone, This Is Our First Question Topic.
+// 测试案例1
+const str1 = 'hello everyOneDay, this is our first question topic.';
+console.log(transferWords1(str1)); // Hello EveryOneDay, This Is Our First Question Topic.
+
+// 方法2：正则
+/**
+ * 将英文中每个单词的首字母都变成大写。
+ * @param {*} str   字符串
+ */
+function transferWords2(str) {
+	if (!str || typeof str !== 'string') return str;
+	return str.replace(/\b([\w|']+)\b/g, function (word) {
+		return word.slice(0, 1).toUpperCase() + word.slice(1);
+	});
+}
+// 测试案例2
+const str2 = 'hello everyOneDay, this is our first question topic.';
+console.log(transferWords2(str2)); // Hello EveryOneDay, This Is Our First Question Topic.

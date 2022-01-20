6d028be4816a3d2023edfd0308c5941b1ddfe8a4
cinyearchan <cinyearchan@gmail.com>
2022-01-20 13:54:36 +0800

    feat: add 16-isFibonacci

---
+++ src/16-0121/isFibonacci-chenzhiwen.js
@@ -0,0 +1,20 @@
+/**
+ * 判断参数数组是否为斐波那契数列
+ * @param {array} array 
+ * @returns {boolean}
+ * @description 斐波那契数列以如下方法定义：F(0)=0，F(1)=1, F(n)=F(n - 1)+F(n - 2)（n ≥ 2，n ∈ N）
+ * @description [0,1,1]为最短有效斐波那契数列
+ */
+const _isFibonacci = array => {
+  // 补全代码
+  const fibFn = (n) => {
+    if (n == 0) {
+      return 0
+    }
+    if (n == 1 || n == 2) {
+      return 1
+    }
+    return fibFn(n -2) + fibFn(n - 1)
+  }
+  return !array.some((item, index) => item !== fibFn(index))
+}

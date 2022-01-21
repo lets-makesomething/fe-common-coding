ac46291629c46ec070e474d52043d546664faa3c
cinyearchan <cinyearchan@gmail.com>
2022-01-21 13:13:23 +0800

    feat: init 17-flatten

---
+++ src/17-0122/flatten-chenzhiwen.js
@@ -0,0 +1,10 @@
+/**
+ * 将数组参数中的多维数组扩展为一维数组并返回该数组
+ * @param {array} arr
+ * @returns {array}
+ * @description 数组参数中仅包含数组类型和数字类型
+ * @description 输入 [1,[2,[3,[4]]]] 输出 [1,2,3,4]
+ */
+const flatten = arr => {
+  // 补全代码
+}

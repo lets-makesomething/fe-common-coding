39f263d3e6af4988bdbbc31dc99517694ecdbe0a
cinyearchan <cinyearchan@gmail.com>
2022-01-25 13:23:59 +0800

    test: 移除二进制对照表简化测试用例

--- test/20-0125/convertToBinary-chenzhiwen.test.js
+++ test/20-0125/convertToBinary-chenzhiwen.test.js
@@ -1,5 +1,4 @@
 import convertToBinary from "../../src/20-0125/convertToBinary-chenzhiwen"
-import { negativeList, notNegativeList } from "./binaryTable"
 
 describe("测试转换为8位二进制数字字符", () => {
   // 测试类型守卫
@@ -19,15 +18,13 @@ describe("测试转换为8位二进制数字字符", () => {
     expect(convertToBinary(-0)).toBe("00000000")
   })
 
-  notNegativeList.forEach(item => {
-    test(`测试非负整数转换 ${item.num}`, () => {
-      expect(convertToBinary(item.num)).toBe(item.binary)
-    })
+  test("测试非负整数转换", () => {
+    expect(convertToBinary(1)).toBe("00000001")
+    expect(convertToBinary(127)).toBe("01111111")
   })
 
-  negativeList.forEach(item => {
-    test(`测试负整数转换 ${item.num}`, () => {
-      expect(convertToBinary(item.num)).toBe(item.binary)
-    })
+  test("测试负整数转换", () => {
+    expect(convertToBinary(-1)).toBe("11111111")
+    expect(convertToBinary(-128)).toBe("10000000")
   })
 })

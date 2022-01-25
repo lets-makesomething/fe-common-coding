410b6aa66dfa29f1b115ca81a1662e4b7f778b50
cinyearchan <cinyearchan@gmail.com>
2022-01-25 09:56:15 +0800

    init 21-0126

---
+++ src/21-0126/containsRepeatingLetter-chenzhiwen.js
@@ -0,0 +1,9 @@
+/**
+ * 给定字符串 str，检查其是否包含连续重复的字母（a-zA-Z），包含返回 true，否则返回 false
+ * @param {string} str 
+ * @return {boolean}
+ * @description 输入 abba 输出 true
+ */
+function containsRepeatingLetter(str) {
+
+}

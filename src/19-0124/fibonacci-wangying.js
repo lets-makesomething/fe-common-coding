4608fe178754ee7bf22197c774b021d68184a154
wangying <2739548987@qq.com>
2022-01-25 18:09:04 +0800

    19-0124-fibonacci-wangying

---
+++ src/19-0124/fibonacci-wangying.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {*} n
+ * 实现斐波那契数列函数,返回第n个斐波那契数。 f(1) = 1, f(2) = 1 等
+ */
+
+function fibonacci(n) {
+   if(n === 1 || n === 2){
+     return 1
+   }
+   return fibonacci(n-1) + fibonacci(n-2)
+}
+

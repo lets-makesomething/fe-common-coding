37585b78725cfcc5248c82de4766a186d09e08ed
cinyearchan <cinyearchan@gmail.com>
2022-01-26 16:31:01 +0800

    test: 添加对特殊字符判断的测试

--- test/21-0126/containsRepeatingLetter-chenzhiwen.test.js
+++ test/21-0126/containsRepeatingLetter-chenzhiwen.test.js
@@ -2,26 +2,32 @@ import containsRepeatingLetter from "../../src/21-0126/containsRepeatingLetter-c
 
 describe("测试检测是否存在连续重复字符", () => {
   test("检测是否有类型守卫", () => {
-    expect(() => containsRepeatingLetter()).toThrow()
-    expect(() => containsRepeatingLetter(123)).toThrow()
-    expect(() => containsRepeatingLetter([])).toThrow()
-    expect(() => containsRepeatingLetter(new Date())).toThrow()
-    expect(() => containsRepeatingLetter({ a: 1 })).toThrow()
-    expect(() => containsRepeatingLetter(true)).toThrow()
-    expect(() => containsRepeatingLetter(null)).toThrow()
-  })
+    expect(() => containsRepeatingLetter()).toThrow();
+    expect(() => containsRepeatingLetter(123)).toThrow();
+    expect(() => containsRepeatingLetter([])).toThrow();
+    expect(() => containsRepeatingLetter(new Date())).toThrow();
+    expect(() => containsRepeatingLetter({ a: 1 })).toThrow();
+    expect(() => containsRepeatingLetter(true)).toThrow();
+    expect(() => containsRepeatingLetter(null)).toThrow();
+  });
 
   test("测试能够检测出连续重复", () => {
-    expect(containsRepeatingLetter('abcdeefghhijklmn')).toBe(true)
-    expect(containsRepeatingLetter('abba')).toBe(true)
-  })
+    expect(containsRepeatingLetter("abcdeefghhijklmn")).toBe(true);
+    expect(containsRepeatingLetter("abba")).toBe(true);
+  });
 
   test("测试函数能否检测没有连续重复的字符串", () => {
-    expect(containsRepeatingLetter('abcdefghijklmn')).toBe(false)
-  })
+    expect(containsRepeatingLetter("abcdefghijklmn")).toBe(false);
+  });
 
-  test("测试函数能够检测大小写", () => {
-    expect(containsRepeatingLetter('abcDdefg')).toBe(false)
-    expect(containsRepeatingLetter('-)?Hh]')).toBe(false)
-  })
-})
+  test("测试函数能够检测重复的大小写字母", () => {
+    expect(containsRepeatingLetter("abcDdefg")).toBe(false);
+    expect(containsRepeatingLetter("-)?Hh]")).toBe(false);
+  });
+
+  test("测试函数能够检测重复的特殊字符", () => {
+    expect(containsRepeatingLetter("[[cdef-+")).toBe(true);
+    expect(containsRepeatingLetter("+ijh++iu[/")).toBe(true);
+    expect(containsRepeatingLetter("//2123")).toBe(true);
+  });
+});

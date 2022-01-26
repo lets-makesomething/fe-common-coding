d71d6256a8c433202f5d804d948063e4e2b78748
bndiya <bndiya@bndiyadeMacBook-Pro.local>
2022-01-26 14:55:38 +0800

    21-0126-containsRepeatingLetter-yipeidong

---
+++ src/21-0126/containsRepeatingLetter-yipeidong.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {*} str 
+ * 给定字符串 str，检查其是否包含连续重复的字母（a-zA-Z），包含返回 true，否则返回 false
+ * input abba
+ * output true
+ */
+
+function containsRepeatingLetter(str) {
+  const regExp = /([a-zA-Z])\1/
+  return regExp.test(str)
+}

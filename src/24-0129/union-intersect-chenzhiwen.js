ce80875df1452aeb23539248c5aed4b74fb9e020
cinyearchan <cinyearchan@gmail.com>
2022-01-28 10:11:13 +0800

    feat: init 24-0129

---
+++ src/24-0129/union-intersect-chenzhiwen.js
@@ -0,0 +1,20 @@
+
+/**
+ * 获取两个数组的并集
+ * @param {array} a 
+ * @param {array} b 
+ * @returns {array}
+ */
+const union = (a,b)=>{
+
+}
+
+/**
+ * 获取两个数组的交集
+ * @param {array} a 
+ * @param {array} b 
+ * @returns {array}
+ */
+const intersect = (a,b)=>{
+  
+}

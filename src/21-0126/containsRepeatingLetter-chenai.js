3ba30830130ef6911ca01dcb59c69458a2346854
aline.chen <aline.chen@dbappsecurity.com.cn>
2022-01-30 19:15:29 +0800

    21-0126-containsRepeatingLetter-chenai

---
+++ src/21-0126/containsRepeatingLetter-chenai.js
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @param {*} str
+ * 给定字符串 str，检查其是否包含连续重复的字母（a-zA-Z），包含返回 true，否则返回 false
+ * input abba
+ * output true
+ */
+function containsRepeatingLetter(str) {
+    return /([a-zA-Z])\1/.test(str)
+
+}
+console.log(containsRepeatingLetter('aaaabb'))
+console.log(containsRepeatingLetter('abba'))
+console.log(containsRepeatingLetter('aba'))

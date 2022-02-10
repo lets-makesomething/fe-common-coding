b209cc94b6a7e6e075dfd4a378c0ea9a1e9c2a26
cinyearchan <cinyearchan@gmail.com>
2022-02-10 11:21:40 +0800

    hotfix: format code

--- src/28-0209/twoSum-chenzhiwen.js
+++ src/28-0209/twoSum-chenzhiwen.js
@@ -1,4 +1,3 @@
-
 /**
  * leetcode 1
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
@@ -29,20 +28,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   for (let i = 0; i < nums.length; i++) {
-      if (nums[i] >= target) {
-         continue
-      } else {
-         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] >= target) {
-               continue
-            }
-            if (nums[j] + nums[i] == target) {
-               return [i, j]
-            }
-         }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) {
+      continue
+    } else {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[j] >= target) {
+          continue
+        }
+        if (nums[j] + nums[i] == target) {
+          return [i, j]
+        }
       }
-   }
+    }
+  }
 }
 
 export default twoSum

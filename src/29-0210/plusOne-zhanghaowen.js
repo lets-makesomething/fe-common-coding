514e15d550948ae51e55faf3e74c105fd9c7f565
zhw <847225523@qq.com>
2022-02-13 16:59:03 +0800

    daily-0210-plusOne-zhanghaowen

---
+++ src/29-0210/plusOne-zhanghaowen.js
@@ -0,0 +1,45 @@
+/**
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+    最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+    你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+     
+
+    示例 1：
+
+    输入：digits = [1,2,3]
+    输出：[1,2,4]
+    解释：输入数组表示数字 123。
+    示例 2：
+
+    输入：digits = [4,3,2,1]
+    输出：[4,3,2,2]
+    解释：输入数组表示数字 4321。
+    示例 3：
+
+    输入：digits = [0]
+    输出：[1]
+     
+
+    提示：
+
+    1 <= digits.length <= 100
+    0 <= digits[i] <= 9
+ */
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  const str = digits.join("");
+  const arr = Number(str) + 1 + "";
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(Number(arr[i]));
+  }
+  return res;
+};
+console.log(plusOne([0]));

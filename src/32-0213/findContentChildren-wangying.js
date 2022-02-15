a8b9f58d8ee488f633d62ff0651f414b5101c66d
wangying <2739548987@qq.com>
2022-02-15 14:28:32 +0800

    32-0213-findContentChildren-wangying

---
+++ src/32-0213/findContentChildren-wangying.js
@@ -0,0 +1,54 @@
+/**
+ * 455. 分发饼干
+    假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+
+    对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；
+    并且每块饼干 j，都有一个尺寸 s[j] 。
+    如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
+    你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+
+    
+    示例 1:
+
+    输入: g = [1,2,3], s = [1,1]
+    输出: 1
+    解释: 
+    你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。
+    虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。
+    所以你应该输出1。
+    示例 2:
+
+    输入: g = [1,2], s = [1,2,3]
+    输出: 2
+    解释: 
+    你有两个孩子和三块小饼干，2个孩子的胃口值分别是1,2。
+    你拥有的饼干数量和尺寸都足以让所有孩子满足。
+    所以你应该输出2.
+    
+
+    提示：
+
+    1 <= g.length <= 3 * 104
+    0 <= s.length <= 3 * 104
+    1 <= g[i], s[j] <= 231 - 1
+ */
+
+/**
+ * @param {number[]} g 胃口值
+ * @param {number[]} s 饼干尺寸
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+   let sum = 0; 
+   const sortF = arr=>arr.sort(function(a, b) {return a-b})
+   const sArr= sortF(s)
+   const gArr = sortF(g)
+   for(let i=0; i<sArr.length; i++){
+      if(sArr[i]>gArr[0]){
+         sum++;
+         gArr.pop()
+      }
+   }
+    return sum
+};
+console.log(findContentChildren([2,9,8,5,13],[10,7,2,6,15]))

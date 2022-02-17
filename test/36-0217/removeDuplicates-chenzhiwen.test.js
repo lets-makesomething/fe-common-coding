92d88c55bb3454c886c8744c7cfb97e80483a4a0
cinyearchan <cinyearchan@gmail.com>
2022-02-17 10:29:42 +0800

    test: 添加边界判断及类型检测测试用例

--- test/36-0217/removeDuplicates-chenzhiwen.test.js
+++ test/36-0217/removeDuplicates-chenzhiwen.test.js
@@ -1,6 +1,20 @@
 import removeDuplicates from '../../src/36-0217/removeDuplicates-chenzhiwen'
 
 describe('test 36-removeDuplicates', () => {
+  test('测试空字符', () => {
+    expect(() => removeDuplicates('')).toThrow()
+  })
+
+  test('测试边界判断', () => {
+    let overLength = 'a'.repeat(20001)
+    expect(() => removeDuplicates(overLength)).toThrow()
+  })
+
+  test('测试字符串只能由小写英文字母组成', () => {
+    let invalidStr = 'ab1c'
+    expect(() => removeDuplicates(invalidStr)).toThrow()
+  })
+
   test('测试整理 abbaca', () => {
     expect(removeDuplicates('abbaca')).toBe('ca')
   })

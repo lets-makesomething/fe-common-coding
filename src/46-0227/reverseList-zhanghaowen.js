c1291c5079e162ce959ef75986742d0f901a6121
zhw <847225523@qq.com>
2022-02-27 17:12:40 +0800

    daily-46-reverseList-zhanghaowen

---
+++ src/46-0227/reverseList-zhanghaowen.js
@@ -0,0 +1,43 @@
+/**
+ * 206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+示例 2：
+
+
+输入：head = [1,2]
+输出：[2,1]
+示例 3：
+
+输入：head = []
+输出：[]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function (head) {
+    let prev = null 
+    let cur = head  
+    while(cur){
+        const next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    }
+    return prev
+};

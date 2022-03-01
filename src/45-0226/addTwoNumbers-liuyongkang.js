13d98f90ec31d99fa281aea07da0a10fdcfe65be
lyka3156 <1210167160@qq.com>
2022-03-01 10:05:27 +0800

    feat: 46-0227-reverseList-liuyongkang

--- src/45-0226/addTwoNumbers-liuyongkang.js
+++ src/45-0226/addTwoNumbers-liuyongkang.js
@@ -88,6 +88,6 @@ var addTwoNumbers = function (l1, l2) {
 };
 
 // 测试
-console.log(addTwoNumbers([2, 4, 3], [5, 6, 4])); // [ 7, 0, 8 ]
-console.log(addTwoNumbers([0], [0])); // [ 0 ]
-console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])); // [ 8, 9, 9, 9, 0, 0, 0, 1 ]
+// console.log(addTwoNumbers([2, 4, 3], [5, 6, 4])); // [ 7, 0, 8 ]
+// console.log(addTwoNumbers([0], [0])); // [ 0 ]
+// console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9])); // [ 8, 9, 9, 9, 0, 0, 0, 1 ]

852f28fbce7aa5ef23dbe83772386be89a4d5a6e
cinyearchan <cinyearchan@gmail.com>
2022-03-03 11:15:10 +0800

    bugfix: 修改子节点遍历方式以及广度优先遍历返回结果

--- src/49-0302/dfs-bfs-chenzhiwen.js
+++ src/49-0302/dfs-bfs-chenzhiwen.js
@@ -35,8 +35,12 @@ let dfs = (root) => {
   let result = []
   ;(function func(root) {
     if (root) result.push(root.val)
-    if (root && root.children[0]) func(root.children[0])
-    if (root && root.children[1]) func(root.children[1])
+    // if (root && root.children[0]) func(root.children[0])
+    // if (root && root.children[1]) func(root.children[1])
+    // 不应该限定 children 的个数
+    root.children.length > 0 && root.children.forEach(child => {
+      func(child)
+    })
   })(root)
   return result
 }
@@ -53,16 +57,21 @@ let bfs = (root) => {
   queue.push(root)
   while (queue.length !== 0) {
     let currentLength = queue.length
-    result.push([])
+    // result.push([])
     for (let i = 0; i < currentLength; i++) {
       let node = queue.shift()
-      result[result.length - 1].push(node.val)
-      if (node.children[0]) {
-        queue.push(node.children[0])
-      }
-      if (node.children[1]) {
-        queue.push(node.children[1])
-      }
+      // result[result.length - 1].push(node.val)
+      result.push(node.val)
+      // if (node.children[0]) {
+      //   queue.push(node.children[0])
+      // }
+      // if (node.children[1]) {
+      //   queue.push(node.children[1])
+      // }
+
+      node.children.length > 0 && node.children.forEach(child => {
+        queue.push(child)
+      })
     }
   }
   return result
